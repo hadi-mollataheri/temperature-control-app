43ce7a29386371872aa6923dab09fe313c89f9c3
Hadi <mhadimollataheri@gmail.com>
2024-04-14 15:22:52 +0330

    Add a final work picture and Create <div>s for basic structure

--- src/App.js
+++ src/App.js
@@ -2,7 +2,18 @@ import React from 'react';
 
 function App() {
   return (
-    <div>Hello world</div>
+    <div id='app-container'>
+
+      <div id='temperature-container'>
+        <div id='temperature'>16℃</div>
+      </div>
+
+      <div id='buttons-container'>
+        <button>+</button>
+        <button>-</button>
+      </div>
+
+    </div>
   );
 }
 
