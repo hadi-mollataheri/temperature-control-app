db0d7d0bb8eca73e447ec0b8fbb01c315509174d
Hadi <mhadimollataheri@gmail.com>
2024-04-15 14:50:36 +0330

    Add conditions for temperature color changing and Limit the value to -10 and +40

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
-import { React, useState } from "react";
+import { React, useState, useEffect } from "react";
 
 function App() {
   // Create a state for the temperature number
   const [temperatureValue, setTemperatureValue] = useState(22);
+  // Create a state for the temperature color
+  const [temperatureColor, setTemperatureColor] = useState("cold");
+
   // Create event handler functions for changing temperatureValue
-  const increaseValue = () => {
+  const increaseValue = (e) => {
     setTemperatureValue((prevTemperatureValue) => {
+      if (prevTemperatureValue === 40) return prevTemperatureValue;
       return prevTemperatureValue + 1;
     });
   };
   const decreaseValue = () => {
     setTemperatureValue((prevTemperatureValue) => {
+      if (prevTemperatureValue === -10) return prevTemperatureValue;
       return prevTemperatureValue - 1;
     });
   };
+  // Apply the corresponding css style(effect) for temperature-display when
+  // value goes upper than 22 and lower than 22
+  useEffect(() => {
+    if (temperatureValue > 22) {
+      setTemperatureColor("hot");
+    } else {
+      setTemperatureColor("cold");
+    }
+  }, [temperatureValue]);
 
   return (
     <div
@@ -21,7 +35,9 @@ function App() {
       className="h-400px w-300px bg-silver-blue rounded-20px shadow-temperatureApp"
     >
       <div className="temperature-display-container">
-        <div className="temperature-display">{temperatureValue}℃</div>
+        <div className={`temperature-display ${temperatureColor}`}>
+          {temperatureValue}℃
+        </div>
       </div>
 
       <div className="button-container">
