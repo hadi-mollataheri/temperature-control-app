b1ccd829455061c8fd46074044f360c7038eb9ad
Hadi <mhadimollataheri@gmail.com>
2024-04-15 09:54:19 +0330

    Make buttons dynamic with creating a state for temperature value

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
-import React from 'react';
+import { React, useState } from "react";
 
 function App() {
+  // Create a state for the temperature number
+  const [temperatureValue, setTemperatureValue] = useState(22);
+  // Create event handler functions for changing temperatureValue
+  const increaseValue = () => {
+    setTemperatureValue((prevTemperatureValue) => {
+      return prevTemperatureValue + 1;
+    });
+  };
+  const decreaseValue = () => {
+    setTemperatureValue((prevTemperatureValue) => {
+      return prevTemperatureValue - 1;
+    });
+  };
+
   return (
-    <div id='app-container' className="h-400px w-300px bg-silver-blue rounded-20px shadow-temperatureApp">
-
-      <div className='temperature-display-container'>
-        <div className='temperature-display'>16℃</div>
+    <div
+      id="app-container"
+      className="h-400px w-300px bg-silver-blue rounded-20px shadow-temperatureApp"
+    >
+      <div className="temperature-display-container">
+        <div className="temperature-display">{temperatureValue}℃</div>
       </div>
 
-      <div className='button-container'>
-        <button>+</button>
-        <button>-</button>
+      <div className="button-container">
+        <button onClick={decreaseValue}>-</button>
+        <button onClick={increaseValue}>+</button>
       </div>
-
     </div>
   );
 }
